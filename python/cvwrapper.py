24b42a0348a76d159272513639bdc05f0a76301c
dummyedu <dummyedu@gmail.com>
2019-05-24 12:26:56 +0800

    add python detection

---
+++ python/cvwrapper.py
@@ -0,0 +1,66 @@
+
+import cv2
+import numpy as np
+
+def detectImage(target, template, options):
+  threshold = options.get('threshold')
+  detectType = cv2.TM_CCOEFF_NORMED
+  algo_type = options.get('algo_type')
+  grey = options.get('grey')
+  if algo_type == 1:
+    detectType = cv2.TM_SQDIFF_NORMED
+  h, w = template.shape[:-1]
+  if grey:
+    res = cv2.matchTemplate(cv2.cvtColor(target, cv2.COLOR_BGR2GRAY),
+      cv2.cvtColor(template, cv2.COLOR_BGR2GRAY), detectType)
+  else:
+    res = cv2.matchTemplate(target, template, detectType)
+
+  minMatch = np.min(res)
+  maxMatch = np.max(res)
+  print 'minMatch = ', minMatch, " maxMatch = ", maxMatch
+  threshold = max(maxMatch - 0.1, threshold)
+  added = []
+
+  loc = []
+  if algo_type == 0:
+    loc = np.where(res >= threshold)
+  elif algo_type == 1:
+    loc = np.where(res <= 1 - threshold)
+
+  print 'detectCount = ', len(loc[0])
+
+  if len(loc[0]) > 2000:
+    print 'detect too much, threshold or detectType is wrong.', threshold, detectType
+    return {
+      'width': w,
+      'height': h,
+      'matches': []
+    }
+
+  matches = []
+  for pt in zip(*loc[::-1]):  # Switch collumns and rows
+    if algo_type == 0:
+      matches.append((int(pt[0]), int(pt[1]), (float)(res[pt[1]][pt[0]])))
+      # matches.append({
+      #   'x': int(pt[0]),
+      #   'y': int(pt[1]),
+      #   'v': (float)(res[pt[1]][pt[0]])
+      # })
+    elif algo_type == 1:
+      matches.append((int(pt[0]), int(pt[1]), 1 - (float)(res[pt[1]][pt[0]])))
+      # matches.append({
+      #   'x': int(pt[0]),
+      #   'y': int(pt[1]),
+      #   'v': 1 - (float)(res[pt[1]][pt[0]])
+      # })
+
+  ret = {}
+  ret['width'] = w
+  ret['height'] = h
+  ret['matches'] = matches
+  return ret
+
+def detectImageByPath(target_path, template_path, options):
+  return detectImage(cv2.imread(target_path), cv2.imread(template_path), options)
+
---
+++ python/detect.py
@@ -0,0 +1,92 @@
+import numpy as np
+import cv2
+import cvwrapper
+import os
+import sys
+import json
+
+screenImage = None
+
+templates = []
+dir_path = os.path.dirname(os.path.realpath(__file__))
+names = ['0_red.png', '1_purple.png', '2_blue.png', '3_green.png', '4_gold.png',
+  '5_brown.png', '6_skull.png', '7_block.png', '8_doom.png']
+
+def resizeImg(img, targetSize):
+  ratio = img.shape[0] / float(targetSize)
+  x = (int)(img.shape[1] / ratio)
+  y = (int)(img.shape[0] / ratio)
+
+  img = cv2.resize(img, (x, y))
+  return img
+
+for name in names:
+  filePath = os.path.join(dir_path, "template/" + name)
+  img = cv2.imread(filePath)
+  print img.shape
+  # img = resizeImg(img, 42)
+  templates.append(img)
+
+def detect(imagePath):
+  # 1. read image
+  img = cv2.imread(imagePath)
+  print("original image shape", img.shape)
+
+  # img = cv2.resize(img, (x, y))
+  img = resizeImg(img, 380)
+  print("regular image shape", img.shape)
+
+  # 2. for each all patterns. get result
+  options = {
+    'threshold': 0.5,
+    'algo_type': 0
+  }
+  results = []
+  colors = [
+    (255, 0, 0), (112, 20, 182), (0, 0, 255), (0, 255, 0), (252, 224, 132), (113, 76, 72), (255, 255, 255), (113, 133, 121), (225, 142, 131)
+  ]
+  for i in range(len(colors)):
+    colors[i] = (colors[i][2], colors[i][1], colors[i][0])
+  for i, t in enumerate(templates):
+    r = cvwrapper.detectImage(img, t, options)
+    results.append(r)
+    # for m in r['matches']:
+    #   top_left = (m['x'], m['y'])
+    #   bottom_right = (top_left[0] + t.shape[1], top_left[1] + t.shape[0])
+    #   cv2.rectangle(img, top_left, bottom_right, colors[i], 2)
+
+  # cv2.imwrite('res.png',img)
+
+  # 3. get roi from all patterns.
+  xrange = [1000, -1000]
+  yrange = [1000, -1000]
+  for r in results:
+    for m in r['matches']:
+      if xrange[0] > m[0]:
+        xrange[0] = m[0]
+      if xrange[1] < m[0]:
+        xrange[1] = m[0]
+      if yrange[0] > m[1]:
+        yrange[0] = m[1]
+      if yrange[1] < m[1]:
+        yrange[1] = m[1]
+  cell_size = min((xrange[1] - xrange[0]) / 7, (yrange[1] - yrange[0]) / 7) - 2
+
+  # 4. by the roi to make rough points for each pattern
+  board = np.zeros(64).reshape(8, 8)
+  p = np.zeros(64).reshape(8, 8)
+  for i, r in enumerate(results):
+    for (x, y, v) in r['matches']:
+      index = (y / cell_size, x / cell_size)
+      if v > p[index]:
+        p[index] = v
+        board[index] = i
+
+  return board.reshape(64).astype(int).tolist()
+
+if __name__ == "__main__":
+  if len(sys.argv) > 1:
+    l = detect(sys.argv[1])
+    if len(sys.argv) > 2:
+      with open(sys.argv[2], 'w') as outfile:
+        json.dump(l, outfile)
